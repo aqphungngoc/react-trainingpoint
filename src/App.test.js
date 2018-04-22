ff4459d7eaec2c995452b8ee75ce25f16685913a
aqphungngoc <aqphungngoc@gmail.com>
2018-04-22 20:11:10 +0700

    use coreUI

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+jest.mock('react-chartjs-2', () => ({
+  Line: () => null,
+  Polar: () => null,
+  Pie: () => null,
+  Radar: () => null,
+  Bar: () => null,
+  Doughnut: () => null,
+}));
+
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
